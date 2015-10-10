0eee03f82af34e05dd41946aa68ed733678b11b8
mpintener <mpintener@gmail.com>
2015-10-10 13:00:08 -0300

    :facepunch: Estoy editando TORRE.

--- src/CapaDeEntidades/Torre.java
+++ src/CapaDeEntidades/Torre.java
@@ -3,10 +3,26 @@ package CapaDeEntidades;
 public class Torre extends Ficha {
 
 
-	public boolean validarMovimiento(){
+	public boolean validarMovimiento(Posicion ini, Posicion fin, Ficha f){
+		
+		boolean valido = true;
+		
+		 char origenL = ini.getLetra();
+		 int origenN = ini.getNumero();
+		 
+		 char destinoL = fin.getLetra();
+		 int destinoN = fin.getNumero();
+		 
+		 if (origenL == destinoL && (destinoN<8) && (destinoN>1))
+		 {
+			ini.setLetra(fin.getLetra());
+			ini.setNumero(fin.getNumero());
+			
+			return valido;
+		 }
 		
 		
 		
-		return false;
+		return false
 	}
 }
