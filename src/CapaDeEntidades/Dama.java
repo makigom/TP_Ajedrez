a83426c099be15aca9c16acbb81fb5e6964a6cb6
mpintener <mpintener@gmail.com>
2015-10-15 14:59:35 -0300

    Hola maki

--- src/CapaDeEntidades/Dama.java
+++ src/CapaDeEntidades/Dama.java
@@ -4,8 +4,14 @@ public class Dama extends Ficha {
 
 	@Override
 	public boolean validarMovimiento(Posicion ini, Posicion fin) {
-		// TODO Auto-generated method stub
-		return false;
+		
+		if ((ini.getLetra() == fin.getLetra()) && (fin.getNumero()<= 8) && (fin.getNumero()>=1) && (ini.getNumero() != fin.getNumero())) return true;
+
+		 else if ((ini.getNumero() == fin.getNumero()) && (fin.getLetra() >= 'a') && (fin.getLetra() <= 'h') && (ini.getLetra() != fin.getLetra())) return true;
+			 
+		if ((ini.getLetra() != fin.getLetra()) && (ini.getNumero() != fin.getNumero()) && (fin.getNumero()<= 8) && (fin.getNumero()>=1) && (fin.getLetra() <= 'a') && (fin.getLetra() >= 'h')) return true;	
+	
+		else return false;
 	}
 
 }
